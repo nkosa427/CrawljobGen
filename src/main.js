c8129cf88b67af0276dd50e18e928f838e4e44e4
nkosa427 <nkosa427@gmail.com>
2021-08-04 21:46:08 -0700

    adjusted window height

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 1000,
+    height: 900,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
